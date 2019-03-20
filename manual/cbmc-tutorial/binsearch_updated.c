c214637d198b59816d7acc11a189fbc538faa90a
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2019-03-20 08:14:34 +0200

    manual: Fix __CPROVER_assume() call in binsearch_updated
    It requires a boolean statement.

--- manual/cbmc-tutorial/binsearch_updated.c
+++ manual/cbmc-tutorial/binsearch_updated.c
@@ -1,8 +1,8 @@
 int binsearch(int x, signed low, signed high) {
   int a[16];
 
-  __CPROVER_assume(low=0);
-  __CPROVER_assume(high=16);
+  __CPROVER_assume(low == 0);
+  __CPROVER_assume(high == 16);
   while(low<high) {
     signed middle=low+((high-low)>>1);
 
