7af72542ee95430e3ecce7827e8ab5791d6c56fa
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2019-03-20 03:12:38 +0200

    manual/cbmc-tutorial: Add binsearch_updated using __CPROVER_assume()

---
+++ manual/cbmc-tutorial/binsearch_updated.c
@@ -0,0 +1,18 @@
+int binsearch(int x, signed low, signed high) {
+  int a[16];
+
+  __CPROVER_assume(low=0);
+  __CPROVER_assume(high=16);
+  while(low<high) {
+    signed middle=low+((high-low)>>1);
+
+    if(a[middle]<x)
+      high=middle;
+    else if(a[middle]>x)
+      low=middle+1;
+    else // a[middle]=x !
+      return middle;
+  }
+
+  return -1;
+}
